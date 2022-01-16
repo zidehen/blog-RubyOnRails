90864196f5695085bc2a3fe5b14808ad8625a6cf
zidehen <zoeidehen@gmail.com>
2022-01-16 17:50:03 +0000

    added article route with default controller and erb files

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get "/articles", to: "articles#index" 
+  #the above route declares that GET /articles requests are mapped to the index action of ArticlesController.
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
